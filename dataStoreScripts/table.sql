87e6b508f0f182781d5a06be6a0f5478386e0e75
JP <john.pillar@rbs.co.uk>
2019-03-12 16:06:37 +0000

    Removed old data scripts

--- dataStoreScripts/table.sql
+++ dataStoreScripts/table.sql
@@ -19,38 +19,3 @@ CREATE TABLE garden_sensor_plant (
   threshold_moisture        DOUBLE PRECISION  NOT NULL,
   threshold_fertilizer      DOUBLE PRECISION  NOT NULL
 )
-
-CREATE TABLE netatmo (
-  time            TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  sender          TEXT              NOT NULL,
-  address         TEXT              NOT NULL,
-  location        TEXT              NULL,
-  battery         INT               NULL,
-  temperature     DOUBLE PRECISION  NULL,
-  humidity        DOUBLE PRECISION  NULL,
-  pressure        DOUBLE PRECISION  NULL,
-  co2             DOUBLE PRECISION  NULL
-)
-
-SELECT create_hypertable('netatmo', 'time', 'address');
-
-CREATE TABLE dyson_purecool (
-  time            TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-  sender          TEXT              NOT NULL,
-  location        TEXT              NOT NULL,
-  air             DOUBLE PRECISION  NULL,
-  temperature     DOUBLE PRECISION  NULL,
-  humidity        DOUBLE PRECISION  NULL,
-  nitrogen        DOUBLE PRECISION  NULL
-)
-
-SELECT create_hypertable('dyson_purecool', 'time', 'location');
-
-CREATE TABLE ios_devices
-(
-    time         timestamp   without time zone  NOT NULL,
-    token        TEXT                           NOT NULL,
-    user         TEXT
-)
-
-SELECT create_hypertable('ios_devices', 'time', 'device_token');
