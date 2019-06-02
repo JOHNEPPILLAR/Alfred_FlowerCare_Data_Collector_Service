619e6fe7ae1bfc405951656fe4387848c2333004
JOHNEPPILLAR <johneppillar@gmail.com>
2019-06-02 08:05:06 -0400

    Updated to node 12, use specific ver in dep

--- api/display/display.js
+++ api/display/display.js
@@ -134,7 +134,7 @@ skill.get('/all', all);
 async function current(req, res, next) {
   serviceHelper.log('trace', 'Display latest garden sensor readings API called');
   try {
-    const SQL = 'SELECT * FROM garden_sensor WHERE time > NOW() - interval \'1 hour\' ORDER BY time LIMIT 1';
+    const SQL = 'SELECT address, sensor_label, plant_name, moisture, threshold_moisture, fertiliser, threshold_fertilizer FROM vw_plant_data ORDER BY sensor_label GROUP BY address, sensor_label, plant_name, moisture, threshold_moisture, fertiliser, threshold_fertilizer';
     serviceHelper.log('trace', 'Connect to data store connection pool');
     const dbClient = await global.devicesDataClient.connect(); // Connect to data store
     serviceHelper.log('trace', 'Get sensor values');
