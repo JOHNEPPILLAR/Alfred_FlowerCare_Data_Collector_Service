8c84863f221b31390143c0e03bf95af3194c887e
JOHNEPPILLAR <johneppillar@gmail.com>
2019-07-07 12:07:51 +0100

    tidy up devices variable

--- collectors/controller.js
+++ collectors/controller.js
@@ -6,13 +6,12 @@ const flowerCare = require('./flowercare/flowercare.js');
 
 exports.collectData = async function FnCollectData() {
   try {
-    const devices = await flowerCare.getFlowerCareDevices(); // Get Flower Care devices
+    let devices = await flowerCare.getFlowerCareDevices(); // Get Flower Care devices
     if (!(devices instanceof Error)) {
       await flowerCare.getFlowerCareData(devices); // Collect and store flower care device data
     }
-    setTimeout(() => {
-      FnCollectData();
-    }, 300000); // Get device data every 5 minutes
+    devices = null;
+    setTimeout(() => FnCollectData(), 300000); // Get device data every 5 minutes
   } catch (err) {
     serviceHelper.log('error', err.message);
   }
