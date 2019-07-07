f8778ec41404a513bc9301dc3d2892c2cc300049
JOHNEPPILLAR <johneppillar@gmail.com>
2019-07-07 11:48:16 +0100

    Removed seperate rescan of devices

--- collectors/controller.js
+++ collectors/controller.js
@@ -4,23 +4,16 @@
 const serviceHelper = require('../lib/helper.js');
 const flowerCare = require('./flowercare/flowercare.js');
 
-let devices;
-
 exports.collectData = async function FnCollectData() {
   try {
-    devices = await flowerCare.getFlowerCareDevices(); // Get Flower Care devices
+    const devices = await flowerCare.getFlowerCareDevices(); // Get Flower Care devices
     if (!(devices instanceof Error)) {
       await flowerCare.getFlowerCareData(devices); // Collect and store flower care device data
-      setTimeout(async () => {
-        await flowerCare.getFlowerCareData(devices); // Collect and store flower care device data
-      }, 300000); // Get device data every 5 minutes
     }
+    setTimeout(() => {
+      FnCollectData();
+    }, 300000); // Get device data every 5 minutes
   } catch (err) {
     serviceHelper.log('error', err.message);
   }
-
-  setTimeout(async () => {
-    devices = null;
-    devices = await flowerCare.getFlowerCareDevices(); // Collect and store flower care device data
-  }, 10800000); // Re-scan/discover devices every 3 hours
 };
