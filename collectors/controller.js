77ac2fa54ee8cd503ad1556141c92d0fac1d9f82
JOHNEPPILLAR <johneppillar@gmail.com>
2019-08-02 20:46:54 +0100

    added ble restart

--- collectors/controller.js
+++ collectors/controller.js
@@ -11,14 +11,20 @@ const serviceHelper = require('../lib/helper.js');
 const timerInterval = 5 * 60 * 1000; // 5 minutes
 
 exports.processFlowerCareDevices = function fnProcessFlowerCareDevices() {
-  serviceHelper.log('trace', 'Starting new client process');
   try {
+    // Restart BLE adaptor
+    serviceHelper.log('trace', 'Power-down bluetooth adapter');
+    fork('hciconfig hci0 down');
+    serviceHelper.log('trace', 'Power-up bluetooth adapter');
+    fork('hciconfig hci0 up');
+
+    serviceHelper.log('trace', 'Starting new client process');
     const childProcess = fork('./collectors/flowercare/flowercare.js');
     childProcess.on('message', (message) => {
       serviceHelper.log('trace', message);
     });
 
-    childProcess.on('close', () => {
+    childProcess.once('close', () => {
       serviceHelper.log('trace', 'Child process complete');
     });
   } catch (err) {
