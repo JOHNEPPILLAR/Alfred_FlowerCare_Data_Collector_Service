8c101806413362b2c9f175537fec34e9fcedb761
JOHNEPPILLAR <johneppillar@gmail.com>
2019-11-09 17:58:28 +0000

    Remove ble restart

--- app/collectors/controller.js
+++ app/collectors/controller.js
@@ -12,21 +12,6 @@ const timerInterval = 5 * 60 * 1000; // 5 minutes
 
 exports.processFlowerCareDevices = async function fnProcessFlowerCareDevices() {
   try {
-    // Restart BLE adaptor
-    serviceHelper.log('trace', 'Power-down bluetooth adapter');
-    await exec('hciconfig hci0 down', (err, stdout, stderr) => {
-      if (err) serviceHelper.log('error', 'node couldn\'t execute the hciconfig hci0 down command');
-      serviceHelper.log('trace', `stdout: ${stdout}`);
-      serviceHelper.log('trace', `stderr: ${stderr}`);
-    });
-
-    serviceHelper.log('trace', 'Power-up bluetooth adapter');
-    await exec('hciconfig hci0 up', (err, stdout, stderr) => {
-      if (err) serviceHelper.log('error', 'node couldn\'t execute the hciconfig hci0 up command');
-      serviceHelper.log('trace', `stdout: ${stdout}`);
-      serviceHelper.log('trace', `stderr: ${stderr}`);
-    });
-
     serviceHelper.log('trace', 'Starting new client process');
     const childProcess = fork('./app/collectors/flowercare/flowercare.js');
     childProcess.on('message', (message) => {
